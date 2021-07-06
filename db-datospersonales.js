994fd63d4f7d9f9c870d59a63b463760ea06921a
sergiorruiz <83740926+sergiorruiz@users.noreply.github.com>
2021-07-06 16:24:35 -0300

    Add files via upload

--- db-datospersonales.js
+++ db-datospersonales.js
@@ -1,4 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require("mongodb").ObjectId;
 
 const url = 'mongodb+srv://SergioRuiz:6jb2eSPHs924RHm7@cluster0.fp9nx.mongodb.net/Taller?retryWrites=true&w=majority';
 const dataBase = 'Taller';
@@ -48,7 +49,54 @@ const leerDatos = (cbErr, cbOk) => {
       });
   }; 
 
+  const editarDatos = (turno,cbErr, cbOk) => {
+  MongoClient.connect(url, { useUnifiedTopology: true }, (err, conok) => {
+      if(err) {
+          cbErr(err);
+          console.log(err)
+          return;
+      };
+  
+      const datospersonalesBase = conok.db(dataBase).collection(collectionName);
+      
+      datospersonalesBase.findOneAndUpdate(
+        {_id: ObjectId(turno.id)},
+        {$set: {costo: turno.costo, estado: turno.estado}}
+        , (err, res) => {
+          if(err){
+            cbErr(err);
+            return;
+          }
+          conok.close();    
+          cbOk();
+        });
+  });
+  }; 
+
+  const eliminarDatos = (id, cbErr, cbOk) => {
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, conok) => {
+      if(err) {
+          cbErr(err);
+          console.log(err)
+          return;
+      };
+  
+      const datospersonalesBase = conok.db(dataBase).collection(collectionName);
+      
+      datospersonalesBase.deleteOne({ _id: ObjectId(id.id) }, (err, result) => {
+        if (err) {
+          cbError(err);
+          return;
+        }
+          conok.close();
+          cbOk();
+        });
+    });
+  };
+
 module.exports = {
   insertarDatos,
-  leerDatos
+  leerDatos,
+  editarDatos,
+  eliminarDatos
 };
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const hdb = require("express-handlebars");
+const nodemailer = require("nodemailer");
 const dbDatosPersonales = require("./db-datospersonales");
 
 
@@ -24,8 +25,65 @@ app.get("/", (req, res) => {
             encodedJson : encodeURIComponent(JSON.stringify(datos))
         })
         )});
-        
 
+app.get("/cliente", (req, res) => {
+    res.render("tarjetacontacto")
+})
+
+//NodeMail
+app.post("/tarjetaContacto", (req, res) => {
+    const salidaEmail = `
+    <p>Notificación por Estado de Reparación</p>
+    <h3>Detalle Contacto</h3>
+    <ul>
+        <li>Nombre: ${req.body.nombre}</li>
+        <li>Email: ${req.body.email}</li>
+    </ul>
+    <h3>Mensaje</h3>
+    <p>${req.body.mensaje}</p>`;
+
+    // create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "sergiorruiz88@gmail.com", // generated ethereal user
+      pass: "ijftbdacoyipwjre", // generated ethereal password
+    },
+    tls:{
+        rejectUnauthorized: false
+    }
+  });
+
+  // send mail with defined transport object
+    let mailOptions = {
+    from: '"Servicio Tecnico ComunidadIT 👻" <sergiorruiz88@gmail.com>', // sender address
+    to: `${req.body.email}`, // list of receivers
+    subject: "Hola ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: salidaEmail, // html body
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+          return console.log(err)
+      }
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    });
+
+res.render("tarjetacontacto", {
+    msj: "Email Enviado"
+})
+
+})
+    
 app.post("/agregar-reparaciones", function(req, res) {
         
     let fecha = req.body.fecha;
@@ -54,7 +112,21 @@ app.post("/agregar-reparaciones", function(req, res) {
         (err) => { res.render("error", {error: "NO INSERTO NADA"})},
         () => {res.redirect("/")});
         
-})    
+})
+
+app.post("/modificarDatos", (req, res) => {
+    const turno = req.body;
+    console.log("turnoModificar", turno);
+    console.log(req.body)
+    dbDatosPersonales.editarDatos(turno,
+        err => res.render("error", { content: "Error"}),
+        datos => res.redirect("/"))
+})
+
+app.get("/eliminarDatos", (req, res) => {
+    const turno = req.body;
+    console.log("eliminar", turno);
+    });
 
 app.listen(3500, () => {
     console.log("Servidor ON")
